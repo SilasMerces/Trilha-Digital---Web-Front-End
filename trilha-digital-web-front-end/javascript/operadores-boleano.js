f3c83148682a422dc9b8c70bd76a04887a5e0519
Silas Mercês <silasmerces1@gmail.com>
2023-11-12 12:54:39 -0300

    Operadores Boleanos

---
+++ trilha-digital-web-front-end/javascript/operadores-boleano.js
@@ -0,0 +1,32 @@
+// Operadores Booleanos
+
+// Igualdede:      == (ou ===)
+// Desigualdade:   != (ou !==)
+// Maior que:       >
+// Maior ou igual:  >=
+// Menor que:       <
+// Menor ou igual:  <=
+
+const numero = 10
+
+console.log(numero == 10)
+console.log(numero > 20)
+
+
+
+// - Conjunções Lógicas
+
+
+// AND => &&
+
+let idade = 26
+let tenhoCNH = true
+
+const possoDirigir = idade >= 18 && tenhoCNH === true
+
+console.log('Posso dirigir?', possoDirigir)
+
+
+// OR => ||
+
+// NOT => !
