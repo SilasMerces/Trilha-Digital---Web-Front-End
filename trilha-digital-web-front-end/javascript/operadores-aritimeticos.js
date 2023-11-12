00be509d8bd3dd8675ab392b219bdaa569068168
Silas Mercês <silasmerces1@gmail.com>
2023-11-12 12:20:18 -0300

    Operadores Aritméticos

---
+++ trilha-digital-web-front-end/javascript/operadores-aritimeticos.js
@@ -0,0 +1,27 @@
+// Operadores Aritméticos
+
+let primeiroNome = 20
+let segundoNome = 2
+
+// Adição:   +
+// Subtração:   -
+// Multiplicação:   *
+// Divisão:   /
+// Exponenciação:   **
+// Resto da divisão:   %
+
+
+console.log('Soma =', primeiroNome + segundoNome)
+console.log('Subtração =', primeiroNome - segundoNome)
+console.log('Multiplicação =', primeiroNome * segundoNome)
+console.log('Divisão =', primeiroNome / segundoNome)
+console.log('Exponenciação =', primeiroNome ** segundoNome)
+console.log('Resto da divisão =', primeiroNome % segundoNome)
+
+
+// Precedência de operadores
+
+console.log(3 + 7 * 3)
+
+// Biblioteca Math
+Math.
