edcc8bed60dd8fa7850b25c92f1aa9e80c96ef8f
Silas Mercês <silasmerces1@gmail.com>
2023-11-12 07:42:00 -0300

    Aprendendo o Básico de Variável

---
+++ trilha-digital-web-front-end/javascript/variaveis.js
@@ -0,0 +1,23 @@
+// Variáveis
+// Tipos primitivos de Variáveis
+// Pedacinhos de memórias que reservamos no computador.
+
+let idade = 30 //Criando a primeira variável
+
+console.log (idade)
+
+idade = 25            // Variável tipo NUMBER
+let numero = 30      // Variável tipo NUMBER
+let altura = 1.80    // Variável tipo NUMBER
+let nome = 'Silas'   // Variável tipo STRING (Texto)
+let estudando = true  // Variável tipo BOLEANO (Verdadeiro ou Falso)
+
+
+console.log(numero)
+console.log(altura)
+console.log(nome)
+console.log(estudando)
+
+let outra; //Se não dermos um valor para variavel, ela recebe Undefined
+
+const constante = 100 // "Variável" constante não pode alterar o valor dela, por isso "const" de constante
