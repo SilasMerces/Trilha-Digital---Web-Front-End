808b81a785226cb75e2d0c1610c4bc302f59033f
Silas Mercês <silasmerces1@gmail.com>
2023-11-14 06:18:43 -0300

    Update App.jsx

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,8 +1,27 @@
 function App() {
   
   return (
-    <h1>Olá, Mundo, React!</h1>
+    <div>
+      <h1>Olá, Mundo, React!</h1>
+      <MeuComponente/>
+      <MeuBotao/>
+    </div>
   )
 }
+
+function MeuBotao() {
+  return(
+    <button>Clique aqui!</button>
+  )
+}
+
+function MeuComponente() {
+  return (
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, aperiam ratione. Sint perferendis nihil maxime rerum odio numquam perspiciatis eligendi laudantium nesciunt, qui aut eveniet tempora animi, exercitationem eius voluptatem.</p>
+  )
+}
+
+
+
 
 export default App
